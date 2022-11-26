9aaf867bafd83111b2e338b98656a0da730bff90
Alan Baines <abaines.vt+github@gmail.com>
2022-11-26 00:39:12 -0500

    cleanup tprint for boolean

--- _Library/_Library.lua
+++ _Library/_Library.lua
@@ -11,14 +11,12 @@
 -- Print contents of `tbl`, with indentation.
 -- `indent` sets the initial level of indentation.
 function tprint (tbl, indent)
-	if not indent then indent = 1 end
+	if not indent then indent = 0 end
 	for k, v in pairs(tbl) do
 		local formatting = string.rep("  ", indent) .. k .. ": "
 		if type(v) == "table" then
 			print(formatting)
 			tprint(v, indent+1)
-		elseif type(v) == 'boolean' then
-			print(formatting .. tostring(v))
 		else
 			print(formatting .. tostring(v))
 		end
