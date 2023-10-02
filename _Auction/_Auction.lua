b63fed781b6e1fd4a9b9794e0ad8a42bc36e7a37
Alan Baines <abaines.vt+github@gmail.com>
2023-10-02 19:22:14 -0400

    local

--- _Auction/_Auction.lua
+++ _Auction/_Auction.lua
@@ -1,6 +1,6 @@
 
 function Auction_OnLoad(...)
-	t_Auction_OnLoad = time()
+	local t_Auction_OnLoad = time()
 
 	AuctionFrame:RegisterEvent("AUCTION_HOUSE_NEW_RESULTS_RECEIVED");
 	AuctionFrame:RegisterEvent("COMMODITY_SEARCH_RESULTS_UPDATED");
@@ -11,15 +11,15 @@ end
 
 
 function Auction_OnEvent(self,event,...)
-	arg1 = select(1,...)
-	arg2 = select(2,...)
-	arg3 = select(3,...)
-	arg4 = select(4,...)
-	arg5 = select(5,...)
-	arg6 = select(6,...)
-	arg7 = select(7,...)
-	arg8 = select(8,...)
-	arg9 = select(9,...)
+	local arg1 = select(1,...)
+	local arg2 = select(2,...)
+	local arg3 = select(3,...)
+	local arg4 = select(4,...)
+	local arg5 = select(5,...)
+	local arg6 = select(6,...)
+	local arg7 = select(7,...)
+	local arg8 = select(8,...)
+	local arg9 = select(9,...)
 
 	if event=="COMMODITY_SEARCH_RESULTS_UPDATED" or event=="ITEM_SEARCH_RESULTS_UPDATED" then
 		SearchResultsUpdated(event,arg1)
