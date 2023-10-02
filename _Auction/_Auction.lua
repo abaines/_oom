b52491d157a304930208a330ea3d2b8294254506
Alan Baines <abaines.vt+github@gmail.com>
2023-10-02 19:53:21 -0400

    ProcessData better
    dead code

--- _Auction/_Auction.lua
+++ _Auction/_Auction.lua
@@ -21,7 +21,7 @@ function Auction_OnEvent(self,event,...)
 	local arg8 = select(8,...)
 	local arg9 = select(9,...)
 
-	if not xtimer("Auction_OnEvent",1) then
+	if not xtimer("Auction_OnEvent",0.5) then
 		return
 	end
 
@@ -44,84 +44,26 @@ function CommoditySearchResultsUpdated(itemID)
 	local data = {}
 	for index = 1, C_AuctionHouse.GetNumCommoditySearchResults(itemID) do
 		local result = C_AuctionHouse.GetCommoditySearchResultInfo(itemID, index)
-		ProcessData(data, result.unitPrice, result.quantity)
+		data[result.unitPrice]=result.quantity
 	end
+	ProcessData(data)
 end
 
 function ItemSearchResultsUpdated(itemKey)
 	local data = {}
 	for index = 1, C_AuctionHouse.GetNumItemSearchResults(itemKey) do
 		local result = C_AuctionHouse.GetItemSearchResultInfo(itemKey, index)
-		ProcessData(data, result.buyoutAmount)
+		local current = data[result.buyoutAmount] or 0
+		data[result.buyoutAmount]=1+current
 	end
+	ProcessData(data)
 end
 
 
-function ProcessData(data,unitPrice,quantity)
-	quantity = quantity or 1
-	print(unitPrice,quantity)
+function ProcessData(data)
+	tprint(data)
 end
 
-
---[[
-function C_AuctionHouse_GetNumSearchResults(event,itemID)
-	if event=="COMMODITY_SEARCH_RESULTS_UPDATED" then
-		return C_AuctionHouse.GetNumCommoditySearchResults(itemID)
-
-	elseif event=="ITEM_SEARCH_RESULTS_UPDATED" then
-		return C_AuctionHouse.GetNumItemSearchResults(itemID)
-
-	else
-		error("unexpected event type")
-	end
-end
-
-function C_AuctionHouse_GetSearchResultInfo(event,itemIDKey, index)
-	if event=="COMMODITY_SEARCH_RESULTS_UPDATED" then
-		local result = C_AuctionHouse.GetCommoditySearchResultInfo(itemIDKey, index)
-		return result.quantity, result.unitPrice
-
-	elseif event=="ITEM_SEARCH_RESULTS_UPDATED" then
-		local result = C_AuctionHouse.GetItemSearchResultInfo(itemIDKey, i)
-		return 1, result.buyoutAmount
-
-	else
-		error("unexpected event type")
-	end
-end
-
-
-function GetEventShort(event)
-	local b,f = string.find(event,"_")
-	local s = string.sub(event,0,f-1)
-	return string.sub(event,0,4)
-end
-
-function SearchResultsUpdated(event,itemIDKey)
-	local num = C_AuctionHouse_GetNumSearchResults(event,itemIDKey)
-	local eventType = GetEventShort(event)
-
-	print("!! ".. num.."  "..eventType)
-
-	for index = 1, num do
-		local result = C_AuctionHouse_GetSearchResultInfo(event,itemIDKey,index)
-	end
-
-end
-
-
-function ProcessCommodity(itemID)
-	print(itemID)
-	local num = C_AuctionHouse.GetNumCommoditySearchResults(itemID)
-	print(num)
-	for i = 1, C_AuctionHouse.GetNumCommoditySearchResults(itemID) do
-		local result = C_AuctionHouse.GetCommoditySearchResultInfo(itemID, i)
-		print(event, itemID, i, result.quantity, result.auctionID, result.unitPrice)
-	end
-end
-
-function ProcessItem(
-]]--
 
 
 
